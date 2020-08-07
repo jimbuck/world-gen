0cff5a21964a84e74d5a61fc23a76bcad4ddc155
Jim Buck <jim@jimbuck.io>
2020-08-07 10:48:39 -0400

    Added `basePath` setting.

--- next.config.js
+++ next.config.js
@@ -2,5 +2,6 @@ const withCSS = require('@zeit/next-css');
 
 module.exports = withCSS({
 	// Required to work with GitHub pages.
-	assetPrefix: process.env.NODE_ENV === 'production' ? '/world-gen' : ''
+	assetPrefix: process.env.NODE_ENV === 'production' ? '/world-gen' : '',
+	basePath: '/world-gen'
 });
